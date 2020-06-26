89cc07713847eb44926f4566f2824b68ba96c57e
DanBourdier <danbourdier@aol.com>
2020-06-25 22:08:50 -0400

    added filter functionality

--- d3code.js
+++ d3code.js
@@ -1,245 +1,200 @@
-// To have options for filters on our vis we can have conditional classNames/outputs that 
-// depend on variables that can act as flags for each attr below.
+
+
+  let radioListener = document.querySelector("div.radio-button-div") 
+  radioListener.addEventListener('click', e => {
+    console.log(e.target.value)
+
+    d3.select("svg").remove();
+    draw(e.target.value)
+
+
+  });
+
+  function draw(filter) {
+    // To have options for filters on our vis we can have conditional classNames/outputs that 
+    // depend on variables that can act as flags for each attr below.
     // E.G:  
-      // let flag = filter1
+    // let flag = filter1
     // .attr(if (flag === filter1) return $$)
 
-// IDEA; have a filter for recovery cases per capita in each state
-const COLORS = ["lightsalmon", "red", "lightcoral", "orangered", "gold", 
-  "darkorange", "khaki", "yellow", "lawngreen", "limegreen", "greenyellow",
-   "mediumseagreen", "cyan", "aquamarine", "mediumturquoise", "deepskyblue",
-    "dodgerblue", "blue", "navy", "mediumslateblue", "fuchsia", "indigo", "ivory",
-     "lavenderblush", "brown", "tan", "slategray", "hotpink", "mediumspringgreen",
+    // IDEA; have a filter for recovery cases per capita in each state
+    const COLORS = ["lightsalmon", "red", "lightcoral", "orangered", "gold",
+      "darkorange", "khaki", "yellow", "lawngreen", "limegreen", "greenyellow",
+      "mediumseagreen", "cyan", "aquamarine", "mediumturquoise", "deepskyblue",
+      "dodgerblue", "blue", "navy", "mediumslateblue", "fuchsia", "indigo", "ivory",
+      "lavenderblush", "brown", "tan", "slategray", "hotpink", "mediumspringgreen",
       "seagreen"]
+    const svg_width = 950;
+    const svg_height = 600;
 
+    let svg = d3.select(".viewbox")
+      .append("svg")
+      .attr("height", svg_height)
+      .attr("width", svg_width)
+    // .append("g")
+    // .attr("transform", "translate(0,0)") // same as margins; just centers our viewbox
+    // .attr("text-anchor", "middle")
 
-
-const svg_width = 950;
-const svg_height = 600;
-
-let svg = d3.select(".viewbox")
-  .append("svg")
-  .attr("height", svg_height)
-  .attr("width", svg_width)
-  // .append("g")
-  // .attr("transform", "translate(0,0)") // same as margins; just centers our viewbox
-  // .attr("text-anchor", "middle")
-
-////////////////////////////////////////////
-// let defs = svg.append("defs")
-
-  //   <feComposite in2="offsetblur" operator="in" />
-  //   <feMerge>
-  //     <feMergeNode />
-  //     <feMergeNode in="SourceGraphic" />
-  //   </feMerge>
-  // </filter >
-
-// defs.append("filter")
-//   .attr("id", "drop-shadow")
-//   .append("feGaussianBlur")
-//     .attr("id", "in")
-//     .attr("stdDeviation", 0.5)
-    // .append("feOffset")
-    //   .attr("dx", 1)
-    //   .attr("dy", 4)
-      // .attr("result", "offsetblur")
-      // .append("feFlood")
-      //   .attr("flood-color", "white")
-        // .append("feComposite")
-        //   .attr("in2", "offsetblur")
-        //   .attr("operator", "in")
-          // .append("feMerge")
-          //   .append("feMergeNode")
-          //   .append("feMergeNode")
-              // .attr("in", "SourceGraphic")
-
-// ABOVE CODE REFERENCED FROM http://bl.ocks.org/wimdows/1502762
-////////////////////////////////////////////////
-
-// we use the below #queue when we have multiple files for use
-d3.queue() // allows us to be able to set gaps in time of execution(TOE)
-  .defer(d3.csv, "data/csse_covid_19_data/csse_covid_19_daily_reports_us/06-20-2020.csv")
-  // defer pretty much allows us halt execution of below until each file with a
+    // ABOVE CODE REFERENCED FROM http://bl.ocks.org/wimdows/1502762
+    ////////////////////////////////////////////////
+    // we use the below #queue when we have multiple files for use
+    d3.queue() // allows us to be able to set gaps in time of execution(TOE)
+      .defer(d3.csv, "data/csse_covid_19_data/csse_covid_19_daily_reports_us/06-20-2020.csv")
+      // defer pretty much allows us halt execution of below until each file with a
       // #defer is loaded.
-  .await(ready) // function that executes (below) upon load
+      .await(vis) // function that executes (below) upon load
+    // let forceX = d3.forceX(svg_width / 2).strength(0.05)
+    ////////////////////////////////////////////////////////////////////////////////
+    let forceStrength = 0.5;
+    let forceXCombine = d3.forceX(svg_width / 2).strength(forceStrength);
 
+    let forceXSplit = d3.forceX(d => {
 
+      if (d.Lat > 30) {
+        return 250
+      } else if (d.Lat < 30) {
+        return 750
+      }
 
-  
-
-// let forceX = d3.forceX(svg_width / 2).strength(0.05)
-
-let forceStrength = 0.5;
-
-////////////////////////////////////////////////////////////////////////////////
-let forceXCombine = d3.forceX(svg_width / 2).strength(forceStrength);
-
-let forceXSplit= d3.forceX(d => {
-  if (d.Lat > 30) {
-    return 250
-  } else if (d.Lat < 30) {
-    return 750
-  }
-
-}).strength(forceStrength);
-
-////////////////////////////////////////////////////////////////////////////////
-
-const charge = d => {
-  return -Math.pow(d.Recovered, 0.739) * forceStrength
-  // return d.Recovered
-}
-// above code reference from D3's sample code for applying charge
-
-// note; d3's force sim works in ticks, meaning nodes don't just
-  // "snap" into place
-let sim = d3.forceSimulation()
-  // it doesnt matter what we name x and y below because they are just 
-    // placeholders
-
-  .force("x", forceXCombine)
-  .force("y", d3.forceY(svg_height / 2).strength(forceStrength))
-    // the collide measurement on line 35 & line 46 must match so that the force between  
+    }).strength(forceStrength);
+    ////////////////////////////////////////////////////////////////////////////////
+    const charge = d => {
+      targetFilter = d[filter];
+      return -Math.pow(d[filter], 0.739) * forceStrength
+    }
+    // above code references from D3's sample code for applying charge
+    ////////////////////////
+    // note; d3's force sim works in ticks, meaning nodes don't just
+    // "snap" into place
+    let sim = d3.forceSimulation()
+      // it doesnt matter what we name x and y below because they are just 
+      // placeholders
+      .force("x", forceXCombine)
+      .force("y", d3.forceY(svg_height / 2).strength(forceStrength))
+      // the collide measurement on line 35 & line 46 must match so that the force between  
       // center points of differing bubbles can be equal to each circles radius (if collision was smaller then circles
-        // would overlap)
+      // would overlap)
+      // .force("collide", d3.forceCollide(d => ( Math.floor(Math.sqrt(d.Recovered) / 6 + 10) )) )
+      .force('charge', d3.forceManyBody().strength(charge))
+    // the above code works better than the one above it!!! REPULSION FTW!!!
+    // had to use sqrt to drastically shrink values over 10000 to proportion    
+    ////////////////////////////////////////////////////////////////////////////////
+    function vis(error, datapoints) {
+      let g = svg.selectAll("g")
+        .data(datapoints)
+        .enter()
+        .append("g")
+        .attr("r", d => (
+          Math.floor(Math.sqrt(d[filter]) / 6 + 10)
+        ))
+        .attr("transform", "translate(0,0)")
 
-  // .force("collide", d3.forceCollide(d => ( Math.floor(Math.sqrt(d.Recovered) / 6 + 10) )) )
-  .force('charge', d3.forceManyBody().strength(charge))
-  // the above code works better than the one above it!!! REPULSION FTW!!!
+      let bubbles = svg.selectAll("g")
+        .append("circle")
+        .attr("class", "state")
+        .attr("r", d => (
+          Math.floor(Math.sqrt(d[filter]) / 6 + 10)
+        )) // our radius of our bubbles
+        .attr("fill", () => {
+          return COLORS[Math.floor(Math.random() * COLORS.length - 1) + 1]
+        })
+        .attr("cx", () => {
+          cxvar = 500
+          return cxvar
+        })
+        .attr("cy", () => {
+          cyvar = 500
+          return cyvar
+        })
+        .on("focus", d => (
+          console.log(d)
+        ))
+      // references above https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/cx
 
-  
-  // had to use sqrt to drastically shrink values over 10000 to proportion
-          
-////////////////////////////////////////////////////////////////////////////////
-
-function ready (error, datapoints) {
-  // datapoints are each object parsed by 
-  let g = svg.selectAll("g")
-  .data(datapoints)
-  .enter()
-  .append("g")
-  // .attr("transform", function (d) { return "translate(" + d.x + ",80)" })
-  // .append("text")
-  .attr("r", d => (
-    Math.floor(Math.sqrt(d.Recovered) / 6 + 10)
-  ))
-  .attr("transform", "translate(0,0)")
-  // .text(d => (d.Province_State))
-
-  let bubbles = svg.selectAll("g")
-      .append("circle")
-      .attr("class", "state")
-      .attr("r", d => (
-        Math.floor(Math.sqrt(d.Recovered) / 6 + 10)
-      )) // our radius of our bubbles
-      .attr("fill", () => {
-        return COLORS[Math.floor(Math.random() * COLORS.length - 1) + 1  ]
-      })
-      .attr("cx", () => {
-        cxvar = 500
-        return cxvar
-      })
-    .attr("cy", () => {
-      cyvar = 500
-      return cyvar
-    })
-    .on("focus", d => (
-      console.log(d)
-    ))
-
-
-    // references above https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/cx
-
-    // perhaps the coolest thing ever! The d3.forceSimulation().nodes
+      // perhaps the coolest thing ever! The d3.forceSimulation().nodes
       // creates points of origination(nodes) that create force against other elements(nodes)
       // reference:: https://medium.com/@bryony_17728/d3-js-two-v-4-network-charts-compared-8d3c66b0499c
       // reference:: https://github.com/d3/d3-force  <--- DENSEEEEE
-  sim.nodes(datapoints)
-    .on('tick', ticked)
+      sim.nodes(datapoints)
+        .on('tick', ticked)
 
+      let words = svg.selectAll("g");
+      words.append("text")
+        .text(d => d.Province_State)
 
-let cxvart;
-let cyvart;
-  
-// let circles = svg.selectAll()
+      let texts = svg.selectAll("text")
 
-let words = svg.selectAll("g");
-  words.append("text")
-    .text(d => d.Province_State)
-    // .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+      function ticked() {
+        bubbles
+          .attr("cx", d => {
+            cxvart = d.x;
+            return d.x
+          })
+          .attr("cy", d => {
+            cyvart = d.y;
+            return d.y
+          });
 
-let texts = svg.selectAll("text")
+        texts
+          .attr("x", d => {
+            console.log(Math.floor(Math.sqrt(d[filter]) / 6 + 10))
+            console.log(d.x)
+            return d.x
+          })
+          .attr("y", d => {
+            // console.log(d.Province_State.length / 2)
+            // console.log(d.r)
+            return d.y
+          })
+          .style("font-size", d => {
+            return (
+              Math.floor(Math.sqrt(d[filter]) / 6 + 10) / 3.14)
+          })
+          .style("text-transform", "uppercase")
+      }
+      // our buttons to override the forcesim factors
+      d3.select(".button-reset")
+        .on("click", () => (
+          sim
+            .force("x", forceXCombine)
+            .alphaTarget(0.20)
+            // https://stackoverflow.com/questions/46426072/what-is-the-difference-between-alphatarget-and-alphamin
+            .restart()
+        ));
 
-// texts._groups[0].forEach(txt => (svg.select(this)
-    //   .attr("x", cxvart)
-    //   .attr("y", cyvart)) )
+      d3.select(".button-split")
+        .on("click", () => (
+          sim
+            .force("x", forceXSplit)
+            .alphaTarget(0.20)
+            .restart()
+        ));
+    }
 
-  function ticked() {
-
-    bubbles
-      .attr("cx", d => {
-        cxvart = d.x;
-        return d.x
-      })
-      .attr("cy", d => {
-        cyvart = d.y;
-        return d.y
-      });
-
-      texts
-        .attr("x", d => {
-
-          return d.x 
-        })
-        .attr("y", d => {
-          // console.log(d.Province_State.length / 2)
-          return d.y
-        })
-        .style("font-size", d => (
-          Math.floor(Math.sqrt(d.Recovered) / 6 + 10) / 3.14
-        ))
-    // texts._groups[0].forEach(txt => (svg.select(this)
-    //   .attr("x", cxvart)
-    //   .attr("y", cyvart)) )
   }
-  // console.log(texts._groups[0][0])
+
+draw("Recovered")
   
-  // we need to iterate thru each text tag inside of EACH circle and alter respective yayayaya
 
 
 
 
 
-  // our buttons to override the forcesim factors
-  d3.select(".button-reset")
-    .on("click", () => (
-      sim
-        .force("x", forceXCombine)
-        .alphaTarget(0.20)
-        // https://stackoverflow.com/questions/46426072/what-is-the-difference-between-alphatarget-and-alphamin
-        .restart()
-    ) );
-
-  d3.select(".button-split")
-    .on("click", () => (
-      sim
-        .force("x", forceXSplit)
-        .alphaTarget(0.20)
-        .restart()
-    ));
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 
-//                       AHA MOMENTS
-// 1. I spent all day debugging why small circles werent separated according to my conditional
-  // its because i couldn't appropiately identify what each circle represented because
-    // of the logic that sizes them defaults values < 200 to be equal to a relatively same 
-      // size as opposed to other circles that had values of 70,000 (we need to add a scale!!!)
 
-// 2. Force charge over force collission because collision acts as barriers preventing
-  // free flowing bubbles! Where as charge acts as a repulsion when set to a negative value!!!
 
-// 3. My ticked method and setting the individual text's positioning
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+  //                       AHA MOMENTS
+  // 1. I spent all day debugging why small circles werent separated according to my conditional
+    // its because i couldn't appropiately identify what each circle represented because
+      // of the logic that sizes them defaults values < 200 to be equal to a relatively same 
+        // size as opposed to other circles that had values of 70,000 (we need to add a scale!!!)
+
+  // 2. Force charge over force collission because collision acts as barriers preventing
+    // free flowing bubbles! Where as charge acts as a repulsion when set to a negative value!!!
+
+  // 3. My ticked method and setting the individual text's positioning
